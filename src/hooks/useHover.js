1f7279a381cf0aef89e54881c3ae7c0fe5845022
Nick <n.t.1996@mail.ru>
2025-11-28 20:59:24 +0400

    feat: implement useHover hook with mouse events

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -4,7 +4,22 @@ export function useHover() {
   const [hovered, setHovered] = useState(false);
   const ref = useRef(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const node = ref.current;
+
+    if (!node) return;
+
+    const handleMouseEnter = () => setHovered(true);
+    const handleMouseLeave = () => setHovered(false);
+
+    node.addEventListener("mouseenter", handleMouseEnter);
+    node.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      node.removeEventListener("mouseenter", handleMouseEnter);
+      node.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
 
   return { hovered, ref };
 }
