d44d4a1d60f43f867c143f1ad02d60edffae8f8e
Nick <n.t.1996@mail.ru>
2025-11-28 20:59:48 +0400

    feat: add Demo component using useHover hook

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useHover } from "./hooks/useHover";
 
 function Demo() {
-  return <div>Здесь будет хук useHover</div>;
+  const { hovered, ref } = useHover();
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        padding: "16px",
+        border: "1px solid #ccc",
+        display: "inline-block",
+      }}
+    >
+      {hovered ? "На меня навели мышку" : "Наведи мышкой на меня"}
+    </div>
+  );
 }
 
 export default Demo;
